935fe5f8a92375004fd0d36a7cee80e6048ac69d
priyapriyam <priyam18@navgurukul.org>
2019-05-18 17:24:27 +0530

    this is hackat

---
+++ friends_list.py
@@ -0,0 +1,18 @@
+friends_list=["priya","anuj","nitesh","dharni","prashant"]
+print friends_list
+friends_list[0]="anmol"
+print friends_list
+friends_list[4]="pawan"
+print len(friends_list)
+
+friends_list.append("prashant")
+print friends_list
+
+friends_list.pop(4)
+print friends_list
+
+friends_list.insert(2,"jyoti") 
+print friends_list
+
+friends_list.remove("dharni")
+print friends_list
---
+++ string_list.py
@@ -0,0 +1,4 @@
+
+banks_list = ["Kotak", "HDFC", "RBL", "SBI", "Bank of Baroda"]
+print banks_list
+print type(banks_list)
